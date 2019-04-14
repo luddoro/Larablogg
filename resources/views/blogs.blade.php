77a878689d8c90ce9a0a776e5d66620d7a49e8d5
Ludvig <luddenr1@live.se>
2019-04-14 20:54:23 +0200

    Fixat ett antal saker till en början

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+@section('content')
+
+<header class="text-center">
+    <h1 class="header-title"> Test för projekt att skapa en blog i laravel </h1>
+        </span>
+    </h1>
+    
+    
+    <div class="main-content">
+        <div class="container">
+            <div class="content">
+                <h3>Basic Routing och setup.</h3>
+                <p>Jag har fixat basic routing till sidan. Fixat en bootstrap navbar samt en login med laravels Auth funktion. Hittils har allting gått bra.</p>
+            </div>
+        </div>
+    </div>
+</header>
+@endsection
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+<!--<footer class="navbar navbar-fixed-bottom">
+        <div class="container text-center">
+            Skapad av Ludvig Björkman 04/14/2019
+        </div>
+    </footer>-->
+    <footer class="navbar fixed-bottom">
+            <div class="container-fluid text-center">
+                <p> Skapad av Ludvig Björkman </p>
+        </div>
+    </footer>
