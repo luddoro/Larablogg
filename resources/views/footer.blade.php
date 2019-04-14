d2065b3bb178f4b7c5d8381023d2000c0cffac60
Ludvig <luddenr1@live.se>
2019-04-14 22:54:05 +0200

    Börjat leka runt i Eloquent. Kan skapa bloggar i konsoll

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,6 +5,6 @@
     </footer>-->
     <footer class="navbar fixed-bottom">
             <div class="container-fluid text-center">
-                <p> Skapad av Ludvig Björkman </p>
+                <p> Sidan är Skapad av Ludvig Björkman.14/04/2019</p>
         </div>
     </footer>
